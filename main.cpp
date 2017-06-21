334f7e9fe153c74fd98b7f8f5f79576a72f67b53
Hải Lê <dark.hades.1102@gmail.com>
2017-06-21 19:36:43 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <iostream>
-// #include <curl/curl.h>
+#include <curl/curl.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
   cout << "There are " << argc << " Arg(s)" << endl;
-  for(int i = 0; i < argc; i++)
+  
+  if(argc == 2)
   {
-    cout << argv[i] << endl;
+    // https://curl.haxx.se/libcurl/c/simple.html
+    for(int i = 0; i < argc; i++)
+    {
+      cout << argv[i] << endl;
+    }
   }
   
   return 0;
