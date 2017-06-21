017eb4602c5be344c8a63a5c8c2d22f3777b6d6a
Hải Lê <dark.hades.1102@gmail.com>
2017-06-21 19:31:00 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  cout << argv[0] << endl;
-  cout << argc << endl;
+  cout << "There are " << argc << " Arg(s)" << endl;
+  for(int i = 0; i < argc; i++)
+  {
+    cout << argv[i] << endl;
+  }
   
   return 0;
 }
