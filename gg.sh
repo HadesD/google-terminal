83856795ed5b7583dd566d8abe188c380b28a8b5
Hải Lê <dark.hades.1102@gmail.com>
2017-06-22 20:17:13 +0900

    Update gg.sh

--- gg.sh
+++ gg.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
-g++ -o gg -std=c++11 \
+app_name = gg
+
+if [ -f $app_name ]; then
+    rm -r $app_name
+fi
+
+g++ -o $app_name -std=c++11 \
   main.cpp \
   -lcurl
 
+if [ ! -f $app_name ]; then
+    exit;
+fi
+
+ # DEBUG
 ./gg
+exit
+
+while true; do
+    read -p "Do you wish to add this program to /bin? (y/n)" yn
+    case $yn in
+        [Yy]* ) make install; ./$app_name; break;;
+        [Nn]* ) ./$app_name;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
